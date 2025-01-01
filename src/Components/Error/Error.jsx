29674ff34c85f38c8eda6e9bd567405d8707741d
SamadhanTikone <samadhantikone003@gmail.com>
2025-01-01 19:48:51 +0530

    404 added

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -3,13 +3,13 @@
 export default function Error() {
 
   return (
-    
 
-    <>
-    
-    <img src="./404.svg" alt="" />
 
-    </>
+    <div className="flex justify-center items-center">
+    
+    <img src="./404 error lost in space-pana.svg" alt="" className="w-[70%] " />
+
+    </div>
 
 
 
