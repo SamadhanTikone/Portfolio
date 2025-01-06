6dcec63741a5093e5a1d79e0e1a1a6e6f10694be
SamadhanTikone <samadhantikone003@gmail.com>
2025-01-06 23:21:55 +0530

    responsive added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -62,12 +62,12 @@ export default function About() {
        <p className="md:text-[22px] div md:mx-6  leading-loose lg:w-[50%]">
        I am currently in my final year of B.Tech in Computer Science and Engineering, where I have developed a strong understanding of a wide range of programming languages, frameworks, and technologies. I am proficient in both front-end and back-end development, with expertise in <span className='text-[#FF6500]'>HTML5, CSS3, Bootstrap, Tailwind, JavaScript, React, Redux, and React Hooks</span> to build dynamic and responsive web applications. </p>
 
-       <img src="./final year.webp" alt="" className="w-[450px] div mb-4" />
+       <img src="./final year.webp" alt="" className="w-[450px]  div mb-4" />
 
        </div>
 
 <div className="flex lg:flex-row flex-col ">
-  <img src="./men.jpeg" alt="" className="lg:w-[450px] mb-12 lg:-mt-12 w-[400px] div1"/>
+  <img src="./men.jpeg" alt="" className="lg:w-[450px] lg:block hidden  mb-12 lg:-mt-12 w-[400px] div1"/>
 <p className="md:text-[22px] md:mx-6 leading-loose float-right lg:w-[50%]  div1 ">
 On the back-end, I have hands-on experience with Core Java, Advanced Java technologies (Servlets, JDBC, JSP), and frameworks like <span className='text-[#FF6500]'>Maven, Hibernate, MicroServices, Spring, and Spring Boot</span>. I am skilled in working with MySQL and other database technologies, and I have built scalable, secure server-side applications. I have also worked with modern web development tools like <span className='text-[#FF6500]'>RESTful APIs, JSON, and AJAX</span> for seamless user interactions and GSAP for high-performance animations.
 </p>
@@ -77,7 +77,7 @@ On the back-end, I have hands-on experience with Core Java, Advanced Java techno
 <p className="md:text-[22px] md:mx-6 leading-loose  lg:w-[50%] mt-4  div1">
 Throughout my academic journey, I have applied these technologies <span className='text-[#FF6500]'>to create practical solutions and innovative projects</span>. I am passionate about learning and applying new technologies, and I continue to seek opportunities to grow as a developer and contribute meaningfully to the field of software engineering.
 </p>
-<img src="./art.jpeg" alt="" className="md:w-[450px] mb-4 lg:-mt-12 mt-4 div1"/>
+<img src="./art.jpeg" alt="" className="md:w-[450px]  mb-4 lg:-mt-12 mt-4 div1"/>
 </div>
 
 
