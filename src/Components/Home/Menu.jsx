10311c135043b4546a0e81d0469a13ee57dd6fb0
SamadhanTikone <samadhantikone003@gmail.com>
2025-01-07 11:20:55 +0530

    new menu added

---
+++ src/Components/Home/Menu.jsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import BadgeIcon from '@mui/icons-material/Badge';
+import MenuIcon from '@mui/icons-material/Menu';
+import FilePresentIcon from '@mui/icons-material/FilePresent';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { Theme } from "../Context/Context";
+
+export default function Menu() {
+
+ const{theme } = useContext(Theme);
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List sx={{marginTop:5}}>
+       
+          <ListItem  disablePadding sx={{margin:"10px 0px"}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon sx={{fontSize:28}}/>
+              </ListItemIcon>
+             
+              <Link to={"/"} className='text-lg uppercase font-semibold'> Home  </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem  disablePadding sx={{margin:"10px 0px"}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <AccountBoxIcon/>
+              </ListItemIcon>
+             
+              <Link to={"/about"} className='text-lg uppercase font-semibold'> About  </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem  disablePadding sx={{margin:"10px 0px"}}>
+            <ListItemButton >
+             
+            <ListItemIcon sx={{opacity:100}}>
+            <FilePresentIcon/>
+            </ListItemIcon>
+                
+             
+             
+              <Link to={"/resume"} className='text-lg uppercase font-semibold'> Resume  </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem  disablePadding sx={{margin:"10px 0px"}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <BadgeIcon/>
+              </ListItemIcon>
+             
+              <Link to={"/portfolio"} className='text-lg uppercase font-semibold'> Portfolio  </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem  disablePadding sx={{margin:"10px 0px"}}>
+            <ListItemButton>
+              <ListItemIcon>
+                <AlternateEmailIcon/>
+              </ListItemIcon>
+             
+              <Link to={"/contact"} className='text-lg uppercase font-semibold'> Contact  </Link>
+            </ListItemButton>
+          </ListItem>
+         
+      
+      </List>
+      <Divider />
+     
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}> <p className={` ${ theme ? "text-white": " text-black" }`}> <MenuIcon sx={{fontSize:36 }} /> </p>  </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -4,8 +4,9 @@ import { useContext, useState } from "react";
 import { Theme } from "../Context/Context";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+
+
+import Menu from "../Home/Menu";
 
 // import { useGSAP } from "@gsap/react"
 // import gsap from "gsap"
@@ -52,12 +53,7 @@ function toggleTheme (){
  
 }
 
-function toggleMenu(){
-   
- 
-        setMenuIsOpen(!menuIsOpen)
- 
-}
+
 
 function toggleClose (){
 
@@ -77,7 +73,8 @@ function toggleClose (){
 
   return (
     <div className={` ${ theme ? "bg-[#222831] text-white" : " bg-white text-[#222831]"} flex justify-around p-6 nav relative `}>
-      <ul className={` ${  menuIsOpen   ? `flex-col absolute w-full z-10 gap-6 text-center top-20 p-12  ${ theme ?  "bg-white text-black opacity-100": " bg-[#222831] text-white" } ` :"hidden " }  flex sm:flex sm:justify-around  sm:flex-row items-center  sm:w-[50%] md:w-[40%]  uppercase font-semibold `}>
+      <p className="md:hidden block"> <Menu /> </p>
+      <ul className={` ${  menuIsOpen   ? `  hidden  ${ theme ? "bg-white text-black opacity-100": " bg-[#222831] text-white" } ` :"hidden " }  flex md:flex md:justify-around  md:flex-row items-center  md:w-[50%]  uppercase font-semibold `}>
         <NavLink to={"/"} onClick={toggleClose} className={({isActive})=> isActive ? "text-[#FF6500]  " : "null"}>Home</NavLink>
         <NavLink to={"/about"} onClick={toggleClose} className={({isActive})=> isActive ? "text-[#FF6500]  " :"null"}>About</NavLink>
         <NavLink to={"/resume"} onClick={toggleClose} className={({isActive})=> isActive ? "text-[#FF6500]  " : "null"}>Resume</NavLink>
@@ -87,9 +84,9 @@ function toggleClose (){
 
        
       </ul>
-      <div className=" sm:hidden" onClick={toggleMenu}>
+      {/* <div className=" sm:hidden" onClick={toggleMenu}>
            { menuIsOpen ?  <CloseIcon sx={{fontSize:"28px"}}/> : <MenuIcon sx={{fontSize:"28px"}}/> }
-        </div>
+        </div> */}
 
 <div className="flex justify-between md:w-[30%] lg:w-[15%]  sm:w-[40%]  w-auto">
 <Button variant="outlined" sx={{color:"#FF6500", borderColor:"#FF6500"}} onClick={handleConnect}> Let&apos;s Connect</Button>
@@ -101,6 +98,8 @@ function toggleClose (){
             > { theme ? <DarkModeIcon/> : <LightModeIcon/> }</Button>
 
 </div>
+
+
 
     </div>
   )
